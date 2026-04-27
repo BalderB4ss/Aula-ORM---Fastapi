b2409e25ba4b85bc1fd8f2326b25eb50ba39b75b
Balder <balder.ansgar@gmail.com>
2026-04-27 16:54:29 -0300

    Paginas html

--- main.py
+++ main.py
@@ -1,43 +1,77 @@
-from fastapi import FastAPI, Depends, HTTPException, Request, Form
+from fastapi import FastAPI, Depends, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from database import get_db
 from models import Curso, Aluno
 
-# pip install jinja2 python-multpart
+#pip install jinja2 python-multipart
+#Inicializar o fastapi
 
-# Inicializar o fastapi
 app = FastAPI(title="Gestão escolar")
 
 templates = Jinja2Templates(directory="templates")
 
-# Rodar api
+#Rota
+#Métodos http: GET(pegar), POST(adicionar), PUT(atualizar), DELETE(deletar)
+
+#Rodar API
 # No terminal: python -m uvicorn main:app --reload
 
-# Rota
-# Métodos http: GET, POST, PUT, DELETE
-
-app.get("/cursos/cadastro")
-def exibir_cadastro(request:Request):
+@app.get("/cursos/cadastro")
+def exibir_cadastro(request: Request):
     return templates.TemplateResponse(
         request,
-        "cadastrar_curso.html",  
-        {"resquest": request}
+        "cadastrar_curso.html",
+        {"request": request}
     )
 
-# Rota para cadastrar um curso
+# rota para cadastrar um curso
 @app.post("/cursos")
 def criar_curso(
-    nome: str= Form(...),
+    nome: str = Form(...),
     carga_horaria: int = Form(...),
     descricao: str = Form(...),
     db: Session = Depends(get_db)
 ):
-    # cadastrar o curso no banco
-    novo_curso = Curso(nome = nome, carga_horaria = carga_horaria, descricao = descricao)
+    
+    #cadastrar o curso no banco
+    novo_curso = Curso(nome=nome, carga_horaria=carga_horaria, descricao=descricao)
     db.add(novo_curso)
     db.commit()
 
-    return RedirectResponse(url="/", status_code=303)
+    return RedirectResponse(url="/cursos", status_code=303)
 
+@app.get("/")
+def exibir_cadastro(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {"request": request}
+    )
+
+@app.get("/listar_cursos")
+def listar_cursos(
+    request: Request,
+    db: Session = Depends(get_db)
+    ):
+
+    cursos = db.query(Curso).all()
+    return templates.TemplateResponse(
+        request,
+        "listar_cursos.html",
+        {"request": request, "cursos": cursos}
+    )
+
+# Rota para deletar
+@app.post("/cursos/{id}/deletar")
+def deletar_curso(
+    id: int,
+    db: Session = Depends(get_db)
+):
+    curso = db.querry(Curso).get(id)
+    if curso:
+        db.delete(curso)
+        db.commit()
+    return RedirectResponse(url="/listar_cursos" status_code=303)
+    
